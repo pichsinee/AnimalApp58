f319382e0a8e741c4478e40f04cf3f7b2ed06f14
pichsinee <p.pichsinee@gmail.com>
2018-03-07 12:20:45 +0700

    playSound Complete

--- app/src/main/java/sdu/cs58/pichsinee/animalapp/GameActivity.java
+++ app/src/main/java/sdu/cs58/pichsinee/animalapp/GameActivity.java
@@ -1,15 +1,72 @@
 package sdu.cs58.pichsinee.animalapp;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class GameActivity extends AppCompatActivity {
+
+    //Explicit
+    Button btn1,btn2,btn3,btn4;
+    ImageView questionImageView;
+    ImageButton volumnImageButton;
+    MediaPlayer mediaPlayer;
+    int questionCount = 1;
+    ArrayList<Integer> qID = new ArrayList<Integer>();
+    String answer;
+    int score = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
+        //Initial view
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+        btn3 = findViewById(R.id.btn3);
+        btn4 = findViewById(R.id.btn4);
+        questionImageView = findViewById(R.id.imvQuestion);
+        volumnImageButton = findViewById(R.id.imbVolumn);
+
+        //แสดงคำถาม
+        for (int i=1; i <= questionCount; i++) {
+            qID.add(i);
+        }
+        Collections.shuffle(qID);   //กำหนดการ random คำถาม
+        setQuestion(qID.remove(0));
     }//end onCreate Method
+
+    private void setQuestion(int qID) { // method setQuestion ทำหน้าที่กำหนดข้อคำถ่มและเฉลยในแต่ละข้อ
+
+        if (qID == 1) {
+            answer = "นก";
+            questionImageView.setImageResource(R.drawable.bird);
+            mediaPlayer = MediaPlayer.create(this, R.raw.bird);
+
+            ArrayList<String> choice = new ArrayList<String>();
+            choice.add("นก");
+            choice.add("ม้า");
+            choice.add("แมว");
+            choice.add("หมู");
+            Collections.shuffle(choice);
+            btn1.setText(choice.remove(0));
+            btn2.setText(choice.remove(0));
+            btn3.setText(choice.remove(0));
+            btn4.setText(choice.remove(0));
+        }
+
+    }//end setQuestion Method
+
+    public void playSound(View view) {
+        mediaPlayer.start();
+    }//end playSound Method
 
 }// end Class
